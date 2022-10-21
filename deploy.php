83649e6fb75c1dabb81bb6f92f95e325a1d6e53a
Handerson Silva <handerson.silva@dotlib.com.br>
2022-10-20 22:11:46 -0300

    [feat] Melhorias no deploy (feature)

--- deploy.php
+++ deploy.php
@@ -1,66 +1,69 @@
 <?php
+
 /** create a file the deploy */
+
 namespace Deployer;
 
 require 'recipe/common.php';
 
-$imageName = 'landpage';
-$version = '1.0.3';
-$port = '8080';
-$replicas = 1;
+const APP = 'SITE-MODE';
+const REPOSITORY = 'git@github.com:HandersonSilva/site-mode.git';
+const NUMBER_RELEASE = 2;
+const AMBIENTE = 'production';
+const IMAGE_NAME = 'site-mode';
+const VERSION = '1.0.3';
+const EXTERNAL_PORT = '8080';
+const PORT = '80';
+const REPLICAS = 1;
 
 inventory('deployment/hosts.yml');
 
-set('default_stage', 'production');
+set('application', APP);
 
-set('application', 'SITE');
+set('repository', REPOSITORY);
+set('keep_releases', NUMBER_RELEASE);
+set('default_stage', AMBIENTE);
+set('default_timeout', 1200);
 
-set('repository', 'git@github.com:HandersonSilva/site-mode.git');
-set('keep_releases', 2);
+task('copy-config', function () {
+	run('cp -f /home/ubuntu/environment/env.feirinha {{release_path}}/.env');
+})->desc('Copy config files');
 
-task('docker-build', function () use ($imageName, $version) {
-    run('cd {{release_path}} && docker build -t handersonsilva/switch-'.$imageName.':'.$version.' .');
-  })->desc('Build image');
+task('docker-build', function () {
+	run('cd {{release_path}} && docker build -t handersonsilva/switch-' . IMAGE_NAME . ':' . VERSION . ' -f ' . AMBIENTE . '.dockerfile .');
+})->desc('Build image');
 
-task('docker-push', function () use ($imageName, $version) {
-    run('docker push handersonsilva/switch-'.$imageName.':'.$version);
-  })->desc('Push image');
+task('docker-push', function () {
+	run('docker push handersonsilva/switch-' . IMAGE_NAME . ':' . VERSION);
+})->desc('Push image');
 
-task('docker-clear-image', function () {
-    run('docker image prune -a -f');
-  })->desc('Push image');
+task('remove-service', function () {
+	try {
+		run("docker service rm " . IMAGE_NAME);
+	} catch (\Exception $e) {
+		write($e->getMessage());
+	}
+	return true;
+})->desc('Remove service');
 
-task('remove-service', function () use ($imageName) {
-    try{
-        run("docker service rm {$imageName}");
-    }catch(\Exception $e){
-        write($e->getMessage());
-    }
-    return true;
-  })->desc('Remove service');
+task('create-service', function () {
+	run("docker service create --name " . IMAGE_NAME . " --with-registry-auth --replicas " . REPLICAS . " -dt -p " . EXTERNAL_PORT . ":" . PORT . " handersonsilva/switch-" . IMAGE_NAME . ":" . VERSION);
+})->desc('Create service');
 
- task('create-service', function () use ($imageName, $version, $port, $replicas) {
-    run("docker service create --name {$imageName} --replicas {$replicas} -dt -p {$port}:80 handersonsilva/switch-{$imageName}:{$version}");
-  })->desc('Create service');
-
-task('test-ssh', function () {
-  run('cd {{release_path}} && touch teste-action.txt');
-})->desc('List files');
-
-task('deploy',[
-    'deploy:info',
-    'deploy:prepare',
-    'deploy:release',
-    'deploy:update_code',
-    'docker-build',
-    'docker-push',
-    'docker-clear-image',
-    'remove-service',
-    'create-service',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:symlink',
-    'cleanup'    
+task('deploy', [
+	'deploy:info',
+	'deploy:prepare',
+	'deploy:release',
+	'deploy:update_code',
+	'copy-config',
+	'docker-build',
+	'docker-push',
+	'remove-service',
+	'create-service',
+	'deploy:shared',
+	'deploy:writable',
+	'deploy:symlink',
+	'cleanup'
 ])->desc('Deploy project');
 
 after('deploy:failed', 'deploy:unlock');
